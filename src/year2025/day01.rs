a7ffa0501a7ed1815658fbdc9f33bdaaf5f13d0f
August Hindenes <august.hindenes@nhn.no>
2025-12-01 09:00:17 +0100

    Parse to signed int for quicker solution time
    By parsing to signed int instead of using unsigned int and tracking left
    or right turn, we save ~10% on part 1 and ~55% on part 2.

--- src/year2025/day01.rs
+++ src/year2025/day01.rs
@@ -1,8 +1,6 @@
 //! # Day 1: Dial Puzzle
 
-type Instruction = (bool, u32);
-
-pub fn parse(input: &str) -> Vec<Instruction> {
+pub fn parse(input: &str) -> Vec<i32> {
     let mut result = Vec::new();
 
     for line in input.lines() {
@@ -18,27 +16,23 @@ pub fn parse(input: &str) -> Vec<Instruction> {
         };
 
         let number: String = chars[1..].iter().collect();
-        if let Ok(steps) = number.parse::<u32>() {
-            result.push((direction, steps));
+        if let Ok(steps) = number.parse::<i32>() {
+            result.push(if direction { steps } else { -steps });
         }
     }
 
     result
 }
 
-pub fn part1(input: &[Instruction]) -> u32 {
+pub fn part1(input: &[i32]) -> u32 {
     let mut dial_position: i32 = 50;
     let mut zero_count: u32 = 0;
 
-    for &(direction, steps) in input {
-        if direction {
-            // Turn right
-            dial_position = (dial_position + steps as i32) % 100;
-        } else {
-            // Turn left (use rem_euclid to handle negative wrap-around)
-            dial_position = (dial_position - steps as i32).rem_euclid(100);
-        }
+    for &steps in input {
+        // Turn dial, we use rem_euclid to handle negative wrap-around
+        dial_position = (dial_position + steps).rem_euclid(100);
 
+        // Check for zero
         if dial_position == 0 {
             zero_count += 1;
         }
@@ -47,12 +41,12 @@ pub fn part1(input: &[Instruction]) -> u32 {
     zero_count
 }
 
-pub fn part2(input: &[Instruction]) -> u32 {
+pub fn part2(input: &[i32]) -> u32 {
     let mut dial_position: i32 = 50;
     let mut zero_count: u32 = 0;
 
-    for &(direction, steps) in input {
-        if direction {
+    for &steps in input {
+        if steps > 0 {
             // We brute force check for crossings
             for step in 1..=steps {
                 let check_position = (dial_position + step as i32) % 100;
